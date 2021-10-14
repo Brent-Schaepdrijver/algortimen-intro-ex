45f9611f1c53d1eb1b9dc04acc2fcd4900298544
JensVA <jens.vanhove99@gmail.com>
2021-10-14 09:53:26 +0200

    added spaces in code to push something

--- exercise1.py
+++ exercise1.py
@@ -3,7 +3,7 @@ Number2 = 10001
 list = []
 numbers = 0
 for number in range(Number1, Number2):
-    if (number%7==0) or (number%9==0):
+    if (number %7 == 0) or (number % 9 == 0):
         list.append(int(number))
         numbers = sum(list)
 print (numbers)
